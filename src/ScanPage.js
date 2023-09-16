b15f03fb290c49d5a001a9dff79d0bcec41dda54
aneil04 <aneil2642@gmail.com>
2023-09-16 16:24:21 -0400

    ui

---
+++ src/ScanPage.js
@@ -0,0 +1,21 @@
+import { Box, Button, Stack, TextField } from "@mui/material";
+import logo from "./logo.png"
+
+export default function ScanPage() {
+  return (
+    <Box sx={{ width: "100%", height: "85vh" }} pt={"20%"} display={"flex"} alignItems={"center"} flexDirection={"column"}>
+      <Stack sx={{ width: "80%", height: "90%" }} direction={"column"} alignItems={"center"} spacing={2}>
+        <Box
+          component="img"
+          alt="The house from the offer."
+          src={logo}
+          sx={{ aspectRatio: 389 / 74, width: 300 }}
+        />
+        <Box sx={{ width: "100%", height: "100%", bgcolor: "lightgray", borderRadius: 2 }} />
+        <Stack direction={"row"} spacing={2}>
+          <Button variant="contained" sx={{ fontWeight: "bold", fontSize: "20px" }} size="large" href="/select">Scan</Button>
+        </Stack>
+      </Stack>
+    </Box>
+  )
+}
